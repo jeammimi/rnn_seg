9e78332f8c06d702c94dd4c7ab545de42f96df02
Arbona <jeanmichel.arbona@gmail.com>
2017-08-16 14:31:55 +0200

    old

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -203,7 +203,7 @@ if __name__ == "__main__":
 
             r = model.fit(data, batch_size=20, nb_epoch=1,
                           callbacks=[Check, Log], validation_split=0.1)
-            print(r)
+            print(r["val_loss"])
 
             if i % 5 == 0:
                 Reduce.model = model
